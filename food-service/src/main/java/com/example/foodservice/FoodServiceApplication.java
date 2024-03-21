b6b022628fd1db9fb01c4e938dd90d3e3e2a466f
Jonathan Chen <sabza178@gmail.com>
2024-03-21 18:22:31 +0700

    Configure Eureka client to connect to local Eureka Server

--- food-service/src/main/java/com/example/foodservice/FoodServiceApplication.java
+++ food-service/src/main/java/com/example/foodservice/FoodServiceApplication.java
@@ -2,10 +2,11 @@ package com.example.foodservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class FoodServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FoodServiceApplication.class, args);
     }
--- order-service/src/main/java/com/example/orderservice/model/Order.java
+++ order-service/src/main/java/com/example/orderservice/model/Order.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(name = "orders")
 public class Order {
 
     @Id
@@ -24,7 +25,7 @@ public class Order {
     @Column(name = "timestamp")
     private LocalDateTime dateTime;
 
-    @OneToMany(mappedBy = "")
+    @OneToMany(mappedBy = "order")
     private List<OrderLineItems> orderLineItemsList;
 
 }
