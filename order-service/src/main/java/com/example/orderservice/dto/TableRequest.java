183593471a835ca151919528158936323501167c
Jonathan Chen <sabza178@gmail.com>
2024-03-22 17:23:31 +0700

    add TableRepository, TableRequest, TableService

---
+++ order-service/src/main/java/com/example/orderservice/dto/TableRequest.java
@@ -0,0 +1,9 @@
+package com.example.orderservice.dto;
+
+import lombok.Data;
+
+@Data
+public class TableRequest {
+    private int table_number;
+    private int capacity;
+}
---
+++ order-service/src/main/java/com/example/orderservice/service/TableKoyamaService.java
@@ -0,0 +1,9 @@
+package com.example.orderservice.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TableKoyamaService {
+
+    public String createTable()
+}
